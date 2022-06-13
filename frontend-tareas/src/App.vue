0d62699e74f705cbb0949a55f19ae2801e029ce7
DrackoFarrely <vi_c_tor01@hotmail.com>
2022-06-13 16:17:45 -0500

    Add files via upload

---
+++ frontend-tareas/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <tarea/>
+</template>
+
+<script>
+import Tarea from './components/Tarea.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+   Tarea
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend-tareas/src/components/Tarea.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <hi class="display-4 text-justify-content-center">Listado de Tareas</hi> 
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-lg-8 ofset-lg-2">
+                <div class="card mt-4">
+                <div class="input-group">
+                    <input type ="text" class="form-control form-control-lg" placeholder="Agregar Tarea" >
+                        <div class="input-group-append">
+                            <button class="btn btn-success btn-lg">Agregar</button>
+                        </div>
+                    
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tarea"
+    }
+</script>
+
+<style  scoped>
+
+</style>
